dfa1e8c9ec791ca727b18d8dd555edcf73b911b9
Kalle Paulsson <kalle.paulsson@skolon.com>
2025-11-03 23:44:49 +0100

    fix nvim updates

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,17 +1,17 @@
 -- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
-  if vim.v.shell_error ~= 0 then
-    vim.api.nvim_echo({
-      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-      { out, "WarningMsg" },
-      { "\nPress any key to exit..." },
-    }, true, {})
-    vim.fn.getchar()
-    os.exit(1)
-  end
+    local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+    local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+    if vim.v.shell_error ~= 0 then
+        vim.api.nvim_echo({
+            { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+            { out,                            "WarningMsg" },
+            { "\nPress any key to exit..." },
+        }, true, {})
+        vim.fn.getchar()
+        os.exit(1)
+    end
 end
 vim.opt.rtp:prepend(lazypath)
 
@@ -34,74 +34,77 @@ vim.opt.showmode = false
 
 require("lazy").setup({
     spec = {
-    { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
-          { "nvim-lualine/lualine.nvim", dependencies = { 'nvim-tree/nvim-web-devicons' }, opts = {} },
-          { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate",
-            config = function ()
-            local configs = require("nvim-treesitter.configs")
+        { "catppuccin/nvim",           name = "catppuccin",                              priority = 1000 },
+        { "nvim-lualine/lualine.nvim", dependencies = { 'nvim-tree/nvim-web-devicons' }, opts = {} },
+        {
+            "nvim-treesitter/nvim-treesitter",
+            build = ":TSUpdate",
+            config = function()
+                local configs = require("nvim-treesitter.configs")
 
-            configs.setup({
-                ensure_installed = {
-                  "bash",
-                  "dockerfile",
-                  "gitcommit",
-                  "gitignore",
-                  "go",
-                  "html",
-                  "java",
-                  "javascript",
-                  "json",
-                  "kotlin",
-                  "lua",
-                  "toml",
-                  "python",
-                  "rust",
-                  "sql",
-                  "typescript",
-                  "vim",
-                  "yaml",
-                },
-                highlight = { enable = true },
-                indent = { enable = true},
-            })
+                configs.setup({
+                    ensure_installed = {
+                        "bash",
+                        "dockerfile",
+                        "gitcommit",
+                        "gitignore",
+                        "go",
+                        "html",
+                        "java",
+                        "javascript",
+                        "json",
+                        "kotlin",
+                        "lua",
+                        "toml",
+                        "python",
+                        "rust",
+                        "sql",
+                        "typescript",
+                        "vim",
+                        "yaml",
+                    },
+                    highlight = { enable = true },
+                    indent = { enable = true },
+                })
             end
-          },
-          {
+        },
+        {
             'nvim-telescope/telescope.nvim', tag = '0.1.8', dependencies = { 'nvim-lua/plenary.nvim' }
-          },
-          { 'alexghergh/nvim-tmux-navigation', config = function()
+        },
+        {
+            'alexghergh/nvim-tmux-navigation',
+            config = function()
+                local nvim_tmux_nav = require('nvim-tmux-navigation')
 
-            local nvim_tmux_nav = require('nvim-tmux-navigation')
+                nvim_tmux_nav.setup {
+                    disable_when_zoomed = true
+                }
 
-            nvim_tmux_nav.setup {
-                disable_when_zoomed = true
-            }
-
-            vim.keymap.set('n', "<C-h>", nvim_tmux_nav.NvimTmuxNavigateLeft)
-            vim.keymap.set('n', "<C-j>", nvim_tmux_nav.NvimTmuxNavigateDown)
-            vim.keymap.set('n', "<C-k>", nvim_tmux_nav.NvimTmuxNavigateUp)
-            vim.keymap.set('n', "<C-l>", nvim_tmux_nav.NvimTmuxNavigateRight)
-            vim.keymap.set('n', "<C-\\>", nvim_tmux_nav.NvimTmuxNavigateLastActive)
-            vim.keymap.set('n', "<C-Space>", nvim_tmux_nav.NvimTmuxNavigateNext)
-
-        end
+                vim.keymap.set('n', "<C-h>", nvim_tmux_nav.NvimTmuxNavigateLeft)
+                vim.keymap.set('n', "<C-j>", nvim_tmux_nav.NvimTmuxNavigateDown)
+                vim.keymap.set('n', "<C-k>", nvim_tmux_nav.NvimTmuxNavigateUp)
+                vim.keymap.set('n', "<C-l>", nvim_tmux_nav.NvimTmuxNavigateRight)
+                vim.keymap.set('n', "<C-\\>", nvim_tmux_nav.NvimTmuxNavigateLastActive)
+                vim.keymap.set('n', "<C-Space>", nvim_tmux_nav.NvimTmuxNavigateNext)
+            end
         }
     },
     { colorscheme = { "catppuccin" } },
-    checker = { enabled = true },
-  })
+    checker = { enabled = false, notify = false },
+})
 
 require("catppuccin").setup({
-  background = {
-    light = "latte",
-    dark = "macchiato"
-  }
+    background = {
+        light = "latte",
+        dark = "macchiato"
+    }
 })
 
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
-vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' }) vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
 vim.keymap.set('n', '<leader>td', builtin.diagnostics, { desc = 'Telescope diagnostics' })
 vim.keymap.set('n', '<leader>tt', ':NvimTreeToggle<cr>')
 vim.cmd.colorscheme "catppuccin"
